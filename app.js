ef452e427b8f55c81fa4ca3afaefc1c092e719ef
unknown <vedkulkarni243@gmail.com>
2025-01-09 11:40:00 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express"); 
+
+const app = express(); 
+
+
+app.get("/", (req, res) => { 
+
+    res.send("Hello, World from Node.js! and Vedant Kulkarni"); 
+
+}); 
+
+
+
+const port = process.env.PORT || 3000; 
+
+app.listen(port, () => { 
+
+    console.log(`Server running at http://localhost:${port}/`); 
+
+}); 
